15ed34df34bd5d2afb631bd91bec4fd1dbce84e3
Jorge Fioranelli <jorge.fioranelli@gmail.com>
2015-05-24 00:07:17 +1000

    Clean up computation expressions

--- FSharp/5.ComputationExpressions.fsx
+++ FSharp/5.ComputationExpressions.fsx
@@ -1,35 +1,8 @@
-﻿#r @"..\packages\FSharpx.Async.1.12.0\lib\net40\FSharpx.Async.dll"
+﻿let isEven number = number % 2 = 0
 
-open System.IO
-open FSharpx.Control
-
-
-let readAsync (file: string) = async {
-    use reader = new StreamReader(file)
-    let! content = reader.AsyncReadToEnd ()
-    content |> printfn "Content: %s" }
-
-let readAsync' (file: string) = async {
-    let! content = async { use reader = new StreamReader(file) 
-                           return! reader.AsyncReadToEnd () }
-    content |> printfn "Content: %s" }
-
-readAsync "C:\Users\Jorge\SkyDrive\Presentations\FiveReasons\FSharp\Data.csv"
-|> Async.RunSynchronously
-
-
-let sequence = seq { 
-    yield 1
-    yield 2
-    yield 3 }
-
-sequence
-|> Seq.iter (fun i -> i |> printfn "%i")
-
-let list = [1;2]
-let sequence' = seq { 
-    yield 1
-    yield! list }
+let x = isEven 1
+let y = isEven 2
+x && y
 
 type LogBuilder() =
     member this.Bind (value, continuation) =
@@ -41,8 +14,23 @@ type LogBuilder() =
 
 let log = LogBuilder()
 
-let isEven number = number % 2 = 0
-
 log { let! x = isEven 1
       let! y = isEven 2
       return x && y }
+
+
+open System.IO
+
+let read (file: string) = 
+    use reader = new StreamReader(file)
+    let content = reader.ReadToEnd ()
+    content |> printfn "Content: %s"
+
+#r @"..\packages\FSharpx.Async.1.12.0\lib\net40\FSharpx.Async.dll"
+
+open FSharpx.Control
+
+let readAsync (file: string) = async {
+    use reader = new StreamReader(file)
+    let! content = reader.AsyncReadToEnd ()
+    content |> printfn "Content: %s" }
